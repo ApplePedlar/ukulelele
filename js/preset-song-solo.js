ffde5f0c59807f99706aac69df8cfd127f048e5f
ApplePedlar <applepedlar@gmail.com>
2015-11-12 03:07:41 +0900

    update preset song for ukulelele-solo

---
+++ js/preset-song-solo.js
@@ -0,0 +1,406 @@
+var notes = [
+  { "note":"    ", "length":24 },
+  
+  { "note":" 300", "length":3 },
+  { "note":"0   ", "length":1 },
+  { "note":" 3  ", "length":2 },
+  
+  { "note":" 000", "length":6 },
+  
+  { "note":" 300", "length":3 },
+  { "note":"0   ", "length":1 },
+  { "note":" 3  ", "length":2 },
+  
+  { "note":" 000", "length":6 },
+  
+  { "note":"5350", "length":4 },
+  { "note":"5   ", "length":2 },
+  
+  { "note":"2120", "length":6 },
+  
+  { "note":"3000", "length":4 },
+  { "note":"3   ", "length":2 },
+  
+  { "note":" 300", "length":6 },
+  
+  { "note":"0102", "length":4 },
+  { "note":"0   ", "length":2 },
+  
+  { "note":"3102", "length":3 },
+  { "note":"2   ", "length":1 },
+  { "note":"0   ", "length":2 },
+  
+  { "note":" 300", "length":3 },
+  { "note":"0   ", "length":1 },
+  { "note":" 3  ", "length":2 },
+  
+  { "note":" 000", "length":6 },
+  
+  { "note":"0102", "length":4 },
+  { "note":"0   ", "length":2 },
+  
+  { "note":"3102", "length":3 },
+  { "note":"2   ", "length":1 },
+  { "note":"0   ", "length":2 },
+  
+  { "note":" 300", "length":3 },
+  { "note":"0   ", "length":1 },
+  { "note":" 3  ", "length":2 },
+  
+  { "note":" 000", "length":6 },
+  
+  { "note":"5350", "length":4 },
+  { "note":"5   ", "length":2 },
+  
+  { "note":"8350", "length":3 },
+  { "note":"5   ", "length":1 },
+  { "note":" 7  ", "length":2 },
+  
+  { "note":"3000", "length":6 },
+  
+  { "note":"7000", "length":6 },
+  
+  { "note":"3000", "length":2 },
+  { "note":" 3  ", "length":2 },
+  { "note":" 0  ", "length":2 },
+  
+  { "note":" 320", "length":3 },
+  { "note":" 1  ", "length":1 },
+  { "note":"  2 ", "length":2 },
+  
+  { "note":"  00", "length":6 },
+  
+  { "note":"2120", "length":2 },
+  { "note":"2120", "length":1 },
+  { "note":"2120", "length":1 },
+  { "note":"2120", "length":1 },
+  { "note":"2120", "length":1 },
+  
+  ////
+  
+  { "note":" 300", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":" 5  ", "length":1 },
+  { "note":" 3  ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 000", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":"3000", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 300", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":" 5  ", "length":1 },
+  { "note":" 3  ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 000", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":"3000", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":"5350", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  5 ", "length":1 },
+  { "note":"535 ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":"2120", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  2 ", "length":1 },
+  { "note":"212 ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":"3000", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":"3000", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 303", "length":2 },
+  { "note":"   3", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":"3003", "length":1 },
+  { "note":"   3", "length":1 },
+  
+  { "note":"0102", "length":2 },
+  { "note":"   2", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":"0102", "length":1 },
+  { "note":"   2", "length":1 },
+  
+  { "note":"3102", "length":2 },
+  { "note":"   2", "length":1 },
+  { "note":"2   ", "length":1 },
+  { "note":"0102", "length":1 },
+  { "note":"   2", "length":1 },
+  
+  { "note":" 300", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":" 5  ", "length":1 },
+  { "note":" 3  ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 000", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":"10  ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":"0100", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":" 4  ", "length":1 },
+  { "note":"010 ", "length":1 },
+  { "note":"   2", "length":1 },
+  
+  { "note":"3100", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"2   ", "length":1 },
+  { "note":"010 ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 300", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":" 5  ", "length":1 },
+  { "note":" 3  ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 000", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":"300 ", "length":2 },
+  
+  { "note":"5350", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  5 ", "length":1 },
+  { "note":"5350", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":"8350", "length":2 },
+  { "note":"  5 ", "length":1 },
+  { "note":"5   ", "length":1 },
+  { "note":"212 ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":"3000", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":" 0  ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":"7000", "length":6 },
+  
+  { "note":"3000", "length":1 },
+  { "note":"   0", "length":1 },
+  { "note":" 3  ", "length":1 },
+  { "note":"   0", "length":1 },
+  { "note":" 0  ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 320", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":" 1  ", "length":1 },
+  { "note":"  2 ", "length":2 },
+  
+  { "note":"  00", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":"30  ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":"2120", "length":2 },
+  { "note":"2120", "length":1 },
+  { "note":"2120", "length":1 },
+  { "note":"2120", "length":2 },
+  
+  ////
+  
+  { "note":" 300", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":" 5  ", "length":1 },
+  { "note":" 3  ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 000", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":"3000", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 300", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":" 5  ", "length":1 },
+  { "note":" 3  ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 000", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":"330 ", "length":2 },
+  
+  { "note":"5350", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  5 ", "length":1 },
+  { "note":"535 ", "length":2 },
+  
+  { "note":"2120", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  2 ", "length":1 },
+  { "note":"212 ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":"3000", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":"3000", "length":1 },
+  { "note":"2000", "length":1 },
+  
+  { "note":"1000", "length":2 },
+  { "note":"1000", "length":1 },
+  { "note":"1000", "length":1 },
+  { "note":"1000", "length":2 },
+  
+  { "note":"0100", "length":1 },
+  { "note":" 4  ", "length":1 },
+  { "note":"0   ", "length":1 },
+  { "note":" 4  ", "length":1 },
+  { "note":"0100", "length":2 },
+  
+  { "note":"3100", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"2   ", "length":1 },
+  { "note":"0100", "length":2 },
+  
+  { "note":" 300", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":" 5  ", "length":1 },
+  { "note":" 3  ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 000", "length":1 },
+  { "note":"   0", "length":1 },
+  { "note":"3   ", "length":1 },
+  { "note":"2   ", "length":1 },
+  { "note":"1   ", "length":1 },
+  { "note":"0   ", "length":1 },
+  
+  { "note":"0100", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":" 4  ", "length":1 },
+  { "note":"010 ", "length":1 },
+  { "note":" 4  ", "length":1 },
+  
+  { "note":"3100", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"2100", "length":1 },
+  { "note":"0100", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 300", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":" 5  ", "length":1 },
+  { "note":" 30 ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 000", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":"300 ", "length":2 },
+  
+  { "note":"5350", "length":2 },
+  { "note":"535 ", "length":1 },
+  { "note":"435 ", "length":1 },
+  { "note":"5350", "length":2 },
+  
+  { "note":"8787", "length":3 },
+  { "note":"5454", "length":1 },
+  { "note":"2121", "length":2 },
+  
+  { "note":"3002", "length":1 },
+  { "note":"   2", "length":1 },
+  { "note":"3001", "length":1 },
+  { "note":"   1", "length":1 },
+  { "note":"3000", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":"7565", "length":6 },
+  
+  { "note":"3000", "length":1 },
+  { "note":"   0", "length":1 },
+  { "note":" 30 ", "length":1 },
+  { "note":"   0", "length":1 },
+  { "note":" 00 ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 320", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":" 1  ", "length":1 },
+  { "note":"  2 ", "length":2 },
+  
+  { "note":"  00", "length":3 },
+  { "note":"  0 ", "length":1 },
+  { "note":" 0  ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":"3000", "length":6 },
+  
+  ////
+
+  { "note":"5350", "length":2 },
+  { "note":"5350", "length":1 },
+  { "note":"4350", "length":1 },
+  { "note":"5350", "length":2 },
+  
+  { "note":"8787", "length":1 },
+  { "note":"   7", "length":1 },
+  { "note":"5454", "length":1 },
+  { "note":"   4", "length":1 },
+  { "note":"2121", "length":1 },
+  { "note":"   1", "length":1 },
+  
+  { "note":"3002", "length":1 },
+  { "note":"   2", "length":1 },
+  { "note":"3001", "length":1 },
+  { "note":"   1", "length":1 },
+  { "note":"3000", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":"7565", "length":6 },
+  
+  { "note":"3000", "length":1 },
+  { "note":"   0", "length":1 },
+  { "note":" 3  ", "length":1 },
+  { "note":"   0", "length":1 },
+  { "note":" 0  ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":" 320", "length":2 },
+  { "note":"   0", "length":1 },
+  { "note":" 1  ", "length":1 },
+  { "note":"  2 ", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":"  01", "length":1 },
+  { "note":"   1", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":" 1  ", "length":1 },
+  { "note":"3   ", "length":1 },
+  { "note":"   1", "length":1 },
+  { "note":"  0 ", "length":1 },
+  { "note":" 1  ", "length":1 },
+  { "note":"3   ", "length":1 },
+  { "note":"   1", "length":1 },
+  
+  { "note":"3000", "length":1 },
+  { "note":"   0", "length":1 },
+  { "note":"7000", "length":1 },
+  { "note":"   0", "length":1 },
+  { "note":"A000", "length":1 },
+  { "note":"   0", "length":1 },
+  
+  { "note":"FC00", "length":6 },
+  
+];
